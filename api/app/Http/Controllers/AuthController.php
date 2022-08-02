539851e93786ff5563e272e2beafe21d7d88f5b2
allandereal <ahullan@gmail.com>
2022-08-02 06:55:02 +0300

    create controller methods to handle api requests

---
+++ api/app/Http/Controllers/AuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
+
+class AuthController extends Controller
+{
+    public function signup(SignupRequest $request)
+    {
+        //
+    }
+
+    public function login(LoginRequest $request)
+    {
+        //
+    }
+}
