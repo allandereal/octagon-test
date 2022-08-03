e27f042bfa876615a2c95183632beef1a04fe60b
allandereal <ahullan@gmail.com>
2022-08-03 08:07:49 +0300

    create tests and factory for user login and signup

--- api/tests/Feature/AuthTest.php
+++ api/tests/Feature/AuthTest.php
@@ -2,27 +2,45 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
 {
-    public function test_use_can_signup()
-    {
-        $response = $this->postJson('/api/signup', [
-            'first_name' => 'Allan',
-            'last_name' => 'Ahumuza',
-            'phone' => '0773020863',
-            'password' => 'password'
-        ]);
+    use WithFaker;
+    use DatabaseTransactions;
 
-        $response->assertStatus(200);
+    public function user_can_be_created()
+    {
+        $phone = '+256755337121';
+
+        User::factory()->create(['phone' => $phone]);
+
+        $this->assertDatabaseHas('users', [
+            'phone' => $phone
+        ]);
     }
 
-    public function test_use_can_login()
+    public function test_user_can_signup_using_api()
     {
-        $response = $this->postJson('/api/login', ['phone' => '0773020863', 'password' => 'password']);
+        $user = User::factory()->make(['phone' => '+256773020863']);
+        
+        $response = $this->postJson('/api/signup', $user->getAttributes());
+
+        $response
+        ->assertStatus(201)
+        ->assertJsonPath('data.phone', $user->phone);
+    }
+
+    public function test_user_can_login_using_api()
+    {
+        $phone = '+256755337120';
+
+        User::factory()->create(['phone' => $phone]);
+
+        $response = $this->postJson('/api/login', ['phone' => $phone, 'password' => 'password']);
 
         $response->assertStatus(200);
     }
