e30b54923a5c9b0af30237fdb79354ab3408dc08
allandereal <ahullan@gmail.com>
2022-08-03 08:08:49 +0300

    login authenticated user

--- api/app/Http/Controllers/AuthController.php
+++ api/app/Http/Controllers/AuthController.php
@@ -4,18 +4,41 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\SignupRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class AuthController extends Controller
 {
     public function signup(SignupRequest $request)
     {
-        return response('successfully signed up');
+        $user = User::create([
+            ...$request->only(['first_name', 'last_name', 'phone']),
+            ...['password' => Hash::make($request->password)]
+        ]);
+
+        return new UserResource($user);
     }
 
     public function login(LoginRequest $request)
     {
-        return response('successfully logged in');
+        $user = User::where('phone', $request->phone)->first();
+
+        if(!Hash::check($request->password, $user->makeVisible(['password'])->password)){
+            return response()->json(['login' => 'failed', 'errors' => ['wrong credentials']], 403);
+        }
+        
+        Auth::loginUsingId($user->id);
+
+        session(['token' => Str::random(64), 'user' => $user->id]);
+        
+        return response()->json([
+            'login' => 'successful',
+            'token' => session('token'),
+            'user' => $user->getAttributes()]);
     }
 
     public function profile(Request $request)
