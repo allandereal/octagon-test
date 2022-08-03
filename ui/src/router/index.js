3c2b5598af3aadafa85ca10724e6800a51cbe485
allandereal <ahullan@gmail.com>
2022-08-03 17:18:02 +0300

    add redirection rules to routes

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -2,9 +2,29 @@ import LoginPage from '../components/LoginPage.vue'
 import SignupPage from '../components/SignupPage.vue'
 import ProfilePage from '../components/ProfilePage.vue'
 
+function redirectAuthenticated(to, from, next){
+    if(!localStorage.getItem("authToken") && to.name == 'profile'){
+        next({ name: 'login' })
+    } else if(localStorage.getItem("authToken") && to.name == 'login') {
+        next({ name: 'profile' })
+    } else {
+        next()
+    }
+}
+
 const routes = [
-    { path: '/', component: ProfilePage, name: 'profile' },
-    { path: '/login', component: LoginPage, name: 'login' },
+    { 
+        path: '/',  
+        component: ProfilePage, 
+        name: 'profile',
+        beforeEnter: [redirectAuthenticated]
+    },
+    { 
+        path: '/login', 
+        component: LoginPage, 
+        name: 'login',
+        beforeEnter: [redirectAuthenticated]
+    },
     { path: '/signup', component: SignupPage, name: 'signup' },
 ]
 
